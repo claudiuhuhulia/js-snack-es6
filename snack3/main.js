12a36dbb4061e2bc46a376b96a44d93f82597f45
cloudhuh <claudiu.huhulia@gmail.com>
2023-06-14 15:41:22 +0200

    snack 3 intro

---
+++ snack3/main.js
@@ -0,0 +1,40 @@
+const bikes = [
+    {
+        name: 'bici1',
+        weight: 34
+    },
+     {
+        name: 'bici2',
+        weight: 37
+    },
+     {
+        name: 'bici3',
+        weight: 38
+    },
+     {
+        name: 'bici4',
+        weight: 87
+    },
+     {
+        name: 'bici5',
+        weight: 23
+    },
+     {
+        name: 'bici6',
+        weight: 55
+    },
+     {
+        name: 'bici7',
+        weight: 90
+    },
+     {
+        name: 'bici8',
+        weight: 56
+    },
+     {
+        name: 'bici9',
+        weight: 43
+    },
+]
+
+console.log(bikes)
---
+++ snack3/readme.md
@@ -0,0 +1,11 @@
+# SNACK 3 #
+
+--
+
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: name e weight.
+Stampare in console la bici con peso minore utilizzando il destructuring.
+
+--
+
+- Creo l'array **bikes** con l'oggetto bici composto da nome e peso.
