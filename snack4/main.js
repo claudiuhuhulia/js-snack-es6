aa8048560b23e312a2cf7fa4a99e3ff713b25c72
cloudhuh <claudiu.huhulia@gmail.com>
2023-06-14 18:12:41 +0200

    snack 4 random

--- snack4/main.js
+++ snack4/main.js
@@ -1,61 +1,56 @@
 const teams = [
 {
     name: 'milan', 
-    points: 0,
-    fouls: 0
+    'punti fatti': 0,
+    'falli subiti': 0
 },
 {
     name: 'inter', 
-    points: 0,
-    fouls: 0
+    'punti fatti': 0,
+    'falli subiti': 0
 },
 {
     name: 'juve', 
-    points: 0,
-    fouls: 0
+    'punti fatti': 0,
+    'falli subiti': 0
 },
 {
     name: 'roma', 
-    points: 0,
-    fouls: 0
+    'punti fatti': 0,
+    'falli subiti': 0
 },
 {
     name: 'palermo', 
-    points: 0,
-    fouls: 0
+    'punti fatti': 0,
+    'falli subiti': 0
 },
 {
     name: 'reggina', 
-    points: 0,
-    fouls: 0
+    'punti fatti': 0,
+    'falli subiti': 0
 },
 {
     name: 'como', 
-    points: 0,
-    fouls: 0
+    'punti fatti': 0,
+    'falli subiti': 0
 },
 {
     name: 'sassuolo', 
-    points: 0,
-    fouls: 0
+    'punti fatti': 0,
+    'falli subiti': 0
 },
 {
     name: 'cagliari', 
-    points: 0,
-    fouls: 0
+    'punti fatti': 0,
+    'falli subiti': 0
 },
 ]
 
-/* let {points, fouls}= teams;
-
-points = Math.floor(Math.random() * 100);
-fouls = Math.floor(Math.random() * 100); */
-
-
- teams.forEach(({points, fouls}) => {
-    points = Math.floor(Math.random() * 100);
-    fouls = Math.floor(Math.random() * 100);
-
+ teams.forEach((squadra) => {
+    squadra['punti fatti'] = Math.floor(Math.random() * 100);
+    squadra['falli subiti'] = Math.floor(Math.random() * 100);
+ 
 })
  
 console.log(teams)
+
